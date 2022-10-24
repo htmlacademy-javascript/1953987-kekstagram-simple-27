653b99a3886a4d10c669c0fea156439a710a1bb5
Keks <keks@htmlacademy.ru>
2022-10-24 13:44:23 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,14 @@
+const getRandomIntInclusive = function (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export {
+  getRandomIntInclusive
+};
